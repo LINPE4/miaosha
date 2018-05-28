875679ba1624713a49cb0eda43f4ac87795d6685
peter <610002997@qq.com>
2018-05-28 23:31:12 +0800

    3-2  秒杀接口实现

---
+++ miaosha/src/main/java/com/peter/miaosha/service/OrderService.java
@@ -0,0 +1,46 @@
+package com.peter.miaosha.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.peter.miaosha.dao.OrderDao;
+import com.peter.miaosha.domain.MiaoshaOrder;
+import com.peter.miaosha.domain.MiaoshaUser;
+import com.peter.miaosha.domain.OrderInfo;
+import com.peter.miaosha.vo.GoodsVo;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	OrderDao orderDao;
+	
+	public MiaoshaOrder getMiaoshaOrderByUserIdGoodsId(long userId, long goodsId) {
+		return orderDao.getMiaoshaOrderByUserIdGoodsId(userId, goodsId);
+	}
+
+	@Transactional
+	public OrderInfo createOrder(MiaoshaUser user, GoodsVo goods) {
+		OrderInfo orderInfo = new OrderInfo();
+		orderInfo.setCreateDate(new Date());
+		orderInfo.setDeliveryAddrId(0L);
+		orderInfo.setGoodsCount(1);
+		orderInfo.setGoodsId(goods.getId());
+		orderInfo.setGoodsName(goods.getGoodsName());
+		orderInfo.setGoodsPrice(goods.getMiaoshaPrice());
+		orderInfo.setOrderChannel(1);
+		orderInfo.setStatus(0);
+		orderInfo.setUserId(user.getId());
+		long orderId = orderDao.insert(orderInfo);
+		MiaoshaOrder miaoshaOrder = new MiaoshaOrder();
+		miaoshaOrder.setGoodsId(goods.getId());
+		miaoshaOrder.setOrderId(orderId);
+		miaoshaOrder.setUserId(user.getId());
+		orderDao.insertMiaoshaOrder(miaoshaOrder);
+		return orderInfo;
+	}
+	
+}
